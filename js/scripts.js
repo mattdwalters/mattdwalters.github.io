c14b2cf553537298ff4eec0c406556e0e4b024be
Matt Walters <mattdwalters@users.noreply.github.com>
2018-07-14 00:38:06 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+
+    makeActive(".dev-tab", "#development-select");
+    makeActive(".design-tab", "#design-select");
+
+    $(".design").hide()
+    $(".dev-tab").click(function() {
+    	$(".design").fadeOut();
+    	$(".design").hide();
+    	$(".dev").fadeIn();
+    });
+    $(".design-tab").click(function() {
+    	$(".dev").fadeOut();
+    	$(".dev").hide();
+    	$(".design").fadeIn();
+    });
+
+    smoothScroll(".to-about", "#begin-about");
+    smoothScroll(".to-projects", "#begin-projects");
+    smoothScroll(".to-contact", "#begin-contact");
+});
+
+function smoothScroll(trigger, endScroll) {
+	$(trigger).click(function() {
+		$('html, body').animate({
+			scrollTop: $(endScroll).offset().top
+		}, 500);
+	});
+}
+
+function makeActive(trigger, activate) {
+	$(trigger).click(function(){
+        $("#projects-tabs > a > div").each(function(){
+            $(this).removeClass("active");
+        });
+        $(activate).addClass("active"); 
+    });
+}
+
+function openNav(trigger) {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav(trigger) {
+    document.getElementById("myNav").style.height = "0%";
+}
